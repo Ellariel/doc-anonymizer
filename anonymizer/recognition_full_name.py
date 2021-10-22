9ada3af5614a359b74fed35192c7a3be96949bda
Ellariel <ell.science@mail.ru>
2021-10-22 20:29:42 +0500

    Update recognition_full_name.py

--- anonymizer/recognition_full_name.py
+++ anonymizer/recognition_full_name.py
@@ -1,40 +1,47 @@
 import stanza
 stanza.download('ru')
 
-import spacy
-# !python3 -m spacy download ru_core_news_sm
-# !python3 -m spacy download ru_core_news_lg
-
 import re
+import json
+import os
+import pathlib
 
 
-def extract_full_name(corpus, lib='stanza'):
+current_directory = str(pathlib.Path(__file__).parent.resolve())
+with open(pathlib.Path(f'{current_directory}/ner_exceptions.json'), 'r') as file:
+    exceptions_dict = json.load(file)
+
+    
+def extract_full_name(corpus):
     
     corpus = preprocess_text(corpus)
     
-    if lib == 'stanza':
-        result = stanza_process(corpus)
-        
-    elif lib == 'spacy':
-        result = spacy_process(corpus)
+    result = stanza_process(corpus)
+                
+    evaluation = get_evaluation(result)   # 0 = уверенность, 1 = могут быть ошибки
     
     full_name_list = []
     for name in result:
-        if re.findall(r'[А-Я]{1}\.[А-Я]*', str(name)) != []: # если регулярка что-то нашла
-            full_name_list.append(str(name).strip())
-        else:                                     # если регулярка ничего не нашла
-            new_name = re.split(' ', str(name))
-#             new_name = str(name).split(' ')
-            for name_ in new_name:
-                full_name_list.append(str(name_).strip()) # возможно, тут не надо стрипить - показатель куска перенесенного слова
-                       
+        if re.findall(r'[А-Я]{1}\.{1}\s*[А-Я]{1}\.{1}\s*[А-Я]{1}[А-Я|а-я]+', str(name)) != []: # регулярка для поиска имен с инициалами
+            
+            if filter_result(name, 'initials'):
+                full_name_list.append(str(name).strip())    
+            
+        else:                                     # если регулярка ничего не нашла -> полное фио
+            if filter_result(name, 'full'):
+                new_name = re.split(' ', str(name))
+
+                for name_ in new_name:
+                    full_name_list.append(str(name_).strip()) # возможно, тут не надо стрипить - показатель куска перенесенного слова
+                    
     full_name_list = postprocess_text(full_name_list)        
     
-    return full_name_list
+    return full_name_list, evaluation
 
 
 def preprocess_text(corpus):
     new_c = re.sub('[$|@|&|"||]', '', corpus)
+    new_c = new_c.replace('\n\n', ', ')
     new_c = new_c.replace('\n', ' ')
     new_c = new_c.replace('  ', ' ')
     
@@ -60,24 +67,68 @@ def stanza_process(corpus):
     result = [ent.text for ent in doc.ents if ent.type == 'PER']
     return result
     
+
+def get_evaluation(result_list):
     
-def spacy_process(corpus):
-    nlp = spacy.load('ru_core_news_lg')
-    doc = nlp(corpus)
-    result = [ent for ent in doc.ents if ent.label_ == 'PER']
-    return result
+    reg_init = r'[А-Я]{1}\.{1}\s*[А-Я]{1}\.{1}\s*'                                 # регулярка для инициалов
+    reg_full = r'[А-Я]{1}[а-я|А-Я]+\s{1}[А-Я]{1}[а-я|А-Я]+\s{1}[А-Я]{1}[а-я|А-Я]+' # регулярка для полных ФИО
+    
+    evaluation_mark_list = []
+    for entity in result_list:
+        if re.findall(reg_full, entity) != [] or re.findall(reg_init, entity) != []:
+            evaluation_mark_list.append(0)
+        else:
+            evaluation_mark_list.append(1)
+    evaluation_set = set(evaluation_mark_list)
+    
+    if set(evaluation_mark_list) == {0}:
+        evaluation_mark = 0
+    else:
+        evaluation_mark = 1
+            
+    return evaluation_mark
+
+
+def filter_result(entity, name_type):
+    
+    if name_type == 'initials':
+        tmp_entity = entity.lower().strip()
+        tmp_entity = tmp_entity.replace(' ', '')
+        if tmp_entity in exceptions_dict['initials']:
+            return False
+        else:
+            return True
+       
+    elif name_type == 'full':
+        tmp_entity = entity.lower().strip()
+        if tmp_entity in exceptions_dict['full']:
+            return False
+        else:
+            return True
+        
+    elif name_type == 'other':
+        tmp_entity = entity.lower().strip()
+        if tmp_entity in exceptions_dict['other']:
+            return False
+        else:
+            return True    
 
 
 def postprocess_text(old_list):
     old_list = list(filter(None, old_list))
     
-    new_list = []
+    tmp_list = []
     for elem in old_list:
         if len(elem) < 3:
             continue
         elif len(elem) == 3 and re.findall(r'[А-Я]{3}', str(elem)) != []:
             continue
         else:
+            tmp_list.append(elem)
+    
+    new_list = []
+    for elem in tmp_list:
+        if filter_result(elem, 'other'):
             new_list.append(elem)
-            
+    
     return new_list
